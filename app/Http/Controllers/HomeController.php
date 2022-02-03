e6815ca13d2bcc70df116e82acfac2ed2942f97c
DimaStolyarchuk <stolyarchyk1990@gmail.com>
2022-02-03 22:47:25 +0200

    page home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Home;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function home()
     {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('home');
+        $homes = Home::get();
+        return view('pages.home', compact('homes'));
     }
 }
