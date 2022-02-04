5565e33379cac563b269ed3d6dc390be47ecc38a
DimaStolyarchuk <stolyarchyk1990@gmail.com>
2022-02-04 20:21:14 +0200

    edit page about

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -2,14 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\About;
 use Illuminate\Http\Request;
 
 class AboutController extends Controller
 {
     public function about()
     {
-        $abouts = About::get();
-        return view('pages.about', compact('abouts'));
+        return view('pages.about');
     }
 }
