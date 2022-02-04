c0fe4a09bc645619ea737e1f585334c7178e4896
DimaStolyarchuk <stolyarchyk1990@gmail.com>
2022-02-04 20:23:40 +0200

    edit page contact

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,14 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Home;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function home()
     {
-        $homes = Home::get();
-        return view('pages.home', compact('homes'));
+        return view('pages.home');
     }
 }
--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -2,14 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\News;
 use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
     public function news()
     {
-        $news = News::get();
-        return view('pages.news', compact('news'));
+        return view('pages.news');
     }
 }
